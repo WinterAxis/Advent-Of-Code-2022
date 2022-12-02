239a23e5c0145f58c450a74b3bf0331e2adacfa1
WinterAxis <78036640+WinterAxis@users.noreply.github.com>
2022-12-01 19:02:37 -0500

    Day1 Part 1

--- Day1/Day1_P1.py
+++ Day1/Day1_P1.py
@@ -1,1 +1,7 @@
-print("Day 1 Part 1");
+maxTotal = 0
+with open('Day1_input.txt', encoding='utf8') as f:
+  for list in f.read().strip().split('\n\n'):
+    count = sum([int(item) for item in list.split("\n")])
+    if count > maxTotal:
+      maxTotal = count
+print(maxTotal)
